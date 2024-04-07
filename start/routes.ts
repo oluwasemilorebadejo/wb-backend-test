90d0f3df9fc86be0a9ce4f88b4e783d3b770c074
blac <badejo.semilore.7@gmail.com>
2024-04-08 00:55:21 +0100

    group routes together

--- start/routes.ts
+++ start/routes.ts
@@ -9,11 +9,49 @@
 
 import router from '@adonisjs/core/services/router'
 
+router.where('id', {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id),
+})
+
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router
+  .group(() => {
+    router.get('/', async () => {
+      return {
+        books: ['chiamaka', 'chiamanda', 'chiamaka'],
+      }
+    })
+  })
+  .prefix('api/v1/books')
+
+router
+  .group(() => {
+    router.get('/', async () => {
+      return {
+        bookmarks: ['bookmark1', '2', 'bookmark3'],
+      }
+    })
+
+    router.delete('/:id', async ({ params }) => {
+      return {
+        bookmark: `deleted ${params.id}`,
+      }
+    })
+  })
+  .prefix('api/v1/bookmarks')
+
+// router.get('/bookmarks/:id', async ({ params }) => {
+//   return {
+//     id: `${params.id}`,
+//     type: typeof params.id,
+//   }
+// })
 
 router.post('/api/v1/auth/signup', async () => {})
 
