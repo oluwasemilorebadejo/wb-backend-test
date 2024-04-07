81c4bbdcf092332006394177f7859a8e54b11ef2
blac <badejo.semilore.7@gmail.com>
2024-04-07 22:52:30 +0100

    create bookmark model

---
+++ app/models/bookmark.ts
@@ -0,0 +1,19 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Bookmark extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare user_id: number
+
+  @column()
+  declare book_id: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
