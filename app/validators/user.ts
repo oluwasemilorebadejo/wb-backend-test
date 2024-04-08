40683c6b696d6e290152af11a1a2f31f21163690
blac <badejo.semilore.7@gmail.com>
2024-04-08 14:09:00 +0100

    create user validation

---
+++ app/validators/user.ts
@@ -0,0 +1,19 @@
+import vine from '@vinejs/vine'
+
+/**
+ * Validates the create user validator
+ */
+export const createSignupValidator = vine.compile(
+  vine.object({
+    username: vine.string().trim(),
+    email: vine.string().trim().email(),
+    password: vine.string().trim().minLength(8),
+  })
+)
+
+export const createLoginValidator = vine.compile(
+  vine.object({
+    email: vine.string().trim().email(),
+    password: vine.string().trim().minLength(8),
+  })
+)
