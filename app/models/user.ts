5107c99fde115f80a4426cc67f08e1559fb17a72
blac <badejo.semilore.7@gmail.com>
2024-04-11 11:40:37 +0100

    create user and bookmark model

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,11 +1,14 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany, beforeSave } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import Bookmark from './bookmark.js'
+import JWTService from '#services/jwt_service'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
-  uids: ['email'],
+  uids: ['email', 'username'],
   passwordColumnName: 'password',
 })
 
@@ -19,7 +22,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column()
   declare email: string
 
-  @column()
+  @column({ serializeAs: null })
   declare password: string
 
   @column.dateTime({ autoCreate: true })
@@ -27,4 +30,33 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @hasMany(() => Bookmark)
+  declare bookmarks: HasMany<typeof Bookmark>
+
+  // @beforeSave()
+  // static async hashPassword(user: User) {
+  //   if (user.$dirty.password) {
+  //     user.password = await hash.make(user.password)
+  //   }
+  // }
+
+  // static async verifyCredentials(email: string, password: string) {
+  //   const user = await this.query().where('email', email).first()
+  //   if (!user) {
+  //     throw new Error('Invalid user credentials')
+  //   }
+  //   const isValid = await hash.verify(user.password, password)
+  //   if (!isValid) {
+  //     throw new Error('Invalid user credentials')
+  //   }
+  //   return user
+  // }
+
+  generateAuthCookieToken() {
+    return JWTService.encodeAuthCookie({
+      id: this.id,
+      email: this.email,
+    })
+  }
 }
