8f9f7a0d0182691d8101781f45d8343a022cebf5
blac <badejo.semilore.7@gmail.com>
2024-04-11 11:36:26 +0100

    create auth controller

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,49 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+import { createSignupValidator, createLoginValidator } from '#validators/user'
+import User from '#models/user'
+
+export default class AuthController {
+  async register({ request, response }: HttpContext) {
+    const payload = await request.validateUsing(createSignupValidator)
+
+    // Check if User already exists
+    const existingUser = await User.query().where('email', payload.email)
+    if (existingUser) {
+      return response.conflict('User already exists')
+    }
+
+    // Create and save the new User
+    const user = new User()
+    user.username = payload.username
+    user.email = payload.email
+    user.password = payload.password
+
+    await user.save()
+
+    return response.ok({
+      status: 'success',
+      message: 'User sucessfully created',
+      data: {
+        user,
+      },
+    })
+  }
+
+  async login({ request, response }: HttpContext) {
+    const payload = await request.validateUsing(createLoginValidator)
+
+    try {
+      const user = await User.verifyCredentials(payload.email, payload.password)
+      const token = user.generateAuthCookieToken()
+      return response.cookie('auth_token', token)
+    } catch (error) {
+      console.log(error)
+      return response.badRequest('Invalid Credentials')
+    }
+  }
+
+  async logout({ response }: HttpContext) {
+    response.clearCookie('auth_token')
+  }
+}
