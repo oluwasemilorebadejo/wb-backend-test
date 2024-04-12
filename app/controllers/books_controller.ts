ba46ca654990c1b4941ea82983d9b13de87a9c7f
blac <badejo.semilore.7@gmail.com>
2024-04-12 10:51:59 +0100

    implement redis caching

--- app/controllers/books_controller.ts
+++ app/controllers/books_controller.ts
@@ -1,9 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import axios from 'axios'
+import redis from '@adonisjs/redis/services/main'
 
 export default class BooksController {
   async index({ request, response }: HttpContext) {
-    // Check if query q is provided
     const query = request.input('q')
     if (!query) {
       return response.status(400).json({
@@ -12,37 +12,41 @@ export default class BooksController {
       })
     }
 
-    // Pagination parameters from the client
-    const page = Number(request.input('page', 1)) // Default to 1 if not provided
-    const pageSize = Number(request.input('pageSize', 10)) // Default to 10 if not provided
-
-    // Calculate startIndex for Google Books API
+    const page = Number(request.input('page', 1))
+    const pageSize = Number(request.input('pageSize', 10))
     const startIndex = (page - 1) * pageSize
 
+    // Generate a unique redis key
+    const cacheKey = `books-search:${query}:page:${page}:size:${pageSize}`
+
     try {
+      // Try to get cached data
+      const cachedData = await redis.get(cacheKey)
+      if (cachedData) {
+        return response.status(200).json(JSON.parse(cachedData))
+      }
+
+      // If no redis, make API request
       const res = await axios.get(`https://www.googleapis.com/books/v1/volumes`, {
-        params: {
-          q: query,
-          startIndex: startIndex,
-          maxResults: pageSize,
-        },
+        params: { q: query, startIndex, maxResults: pageSize },
       })
 
-      const books = res.data.items
-      const totalItems = res.data.totalItems
-      const totalPages = Math.ceil(totalItems / pageSize)
-
-      return response.status(200).json({
+      const booksData = {
         status: 'success',
         message: 'Books retrieved successfully',
         data: {
           currentPage: page,
           pageSize,
-          totalPages,
-          totalItems,
-          books,
+          totalPages: Math.ceil(res.data.totalItems / pageSize),
+          totalItems: res.data.totalItems,
+          books: res.data.items,
         },
-      })
+      }
+
+      // cache the new data
+      await redis.set(cacheKey, JSON.stringify(booksData), 'EX', 1800) // 1800 seconds = 30 minutes
+
+      return response.status(200).json(booksData)
     } catch (error) {
       console.error(error)
       return response.status(error.response?.status || 500).json({
